d5c5f799a428fd356c15b854791b2e999be2ca68
Madihaali-1 <madiha28580@gmail.com>
2026-01-02 17:15:22 +0500

    change url

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://book-drab-seven.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
